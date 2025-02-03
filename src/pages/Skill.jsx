2c96fade211ba056bc8436b6aedfba6ef811acc7
muhammad rizal <rizal180204@gmail.com>
2025-02-03 18:36:38 +0700

    change Home teks

--- src/pages/Skill.jsx
+++ src/pages/Skill.jsx
@@ -2,7 +2,7 @@ import { CardSkills } from "../components/CardSkills.jsx";
 
 const Skill = () => {
   return (
-    <div id="skill" className="h-screen bg-[#243137] mt-96 lg:mt-72"> 
+    <div id="skill" className="h-screen bg-[#243137] mt-96 lg:mt-72 overflow-x-hidden"> 
       <div className="absolute left-24 h-screen w-[4px] sm:w-[10px] xl:w-7 xl:left-[280px] sm:left-[105px] bg-[#bd9f67]"></div>
 
       <div className="text-[#bd9f67] font-extrabold text-center w-28 bg-[#243137] 
